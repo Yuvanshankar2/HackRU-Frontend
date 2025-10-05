f1290d4a1c000b6bdaf46b734efd56faa3ec3d32
Yuvanshankar2 <135748876+Yuvanshankar2@users.noreply.github.com>
2025-10-05 12:59:47 -0400

    Add files via upload

---
+++ App.tsx
@@ -0,0 +1,36 @@
+import { useState } from "react";
+import { Header } from "./components/Header";
+import { Hero } from "./components/Hero";
+import { Features } from "./components/Features";
+import { HowItWorks } from "./components/HowItWorks";
+import { Footer } from "./components/Footer";
+import { Login } from "./components/Login";
+import { Welcome } from "./components/Welcome";
+
+export default function App() {
+  const [currentPage, setCurrentPage] = useState<"home" | "login" | "welcome">("home");
+
+  const navigateToLogin = () => setCurrentPage("login");
+  const navigateToHome = () => setCurrentPage("home");
+  const navigateToWelcome = () => setCurrentPage("welcome");
+
+  if (currentPage === "login") {
+    return <Login onBackToHome={navigateToHome} onLogin={navigateToWelcome} />;
+  }
+
+  if (currentPage === "welcome") {
+    return <Welcome onBackToHome={navigateToHome} />;
+  }
+
+  return (
+    <div className="min-h-screen flex flex-col">
+      <Header onGetStarted={navigateToLogin} />
+      <main className="flex-1">
+        <Hero onGetStarted={navigateToLogin} />
+        <Features />
+        <HowItWorks />
+      </main>
+      <Footer />
+    </div>
+  );
+}
